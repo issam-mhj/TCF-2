95cd4b1b76c09d7b8b223c60a84a73971f01be09
issam-mhj <issammahtaj02@gmail.com>
2024-11-12 22:46:11 +0100

    tuesday merge

--- js/first.js
+++ js/first.js
@@ -16,3 +16,29 @@ function showLogin() {
     document.getElementById('logOrSign').classList.remove('hidden');
   }
   
+  //function dyal ID
+
+  function generateRandomId() {
+    return '' + Date.now();
+  }
+
+  const nvCompte = document.getElementById("btnSignUP");
+  const userName = document.getElementById("userName");
+  const usersData = window.localStorage;
+  let countUsers = 0;
+
+  //fach ghaykliki 3la sign up
+
+  nvCompte.addEventListener("click", function() {
+    const userValue = userName.value; 
+    usersData.setItem(`user${countUsers+1}`, userValue);
+    usersData.setItem(`userID${countUsers+1}`, generateRandomId());
+    displayquiz();
+    countUsers++;
+  });
+  
+  const userInterface = document.getElementById("userInterface"); 
+  function displayquiz(){ 
+    document.getElementById('signUP').classList.add('hidden');
+    userInterface.style.display = "flex";
+  }
