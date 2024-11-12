15a7a6577b9ae046af781ea5c8963964e8d10222
ikrambenallali <ikrambenallali2003@gmail.com>
2024-11-12 19:48:28 +0100

    questions

--- js/grammaire.js
+++ js/grammaire.js
@@ -326,105 +326,307 @@ correctOption: "Elle n'aime pas la musique.."
     //=========****B2****============//
     
         {
-            "question": "Bien qu'il ____ (savoir) la vérité, il préfère se taire.",
-            "categ": "Grammaire",
-            "NV": "B2",
-            "option1": "sache",
-            "option2": "sait",
-            "option3": "savait",
-            "option4": "saura",
-            "correctOption": "sache"
+            question: "Bien qu'il ____ (savoir) la vérité, il préfère se taire.",
+            categ: "Grammaire",
+            NV: "B2",
+            option1: "sache",
+            option2: "sait",
+            option3: "savait",
+            option4: "saura",
+            correctOption: "sache"
         },
         {
-            "question": "Il est parti sans que nous ____ (avoir) le temps de le saluer.",
-            "categ": "Grammaire",
-            "NV": "B2",
-            "option1": "avons",
-            "option2": "ayons",
-            "option3": "avions",
-            "option4": "aurons",
-            "correctOption": "ayons"
+            question: "Il est parti sans que nous ____ (avoir) le temps de le saluer.",
+            categ: "Grammaire",
+            NV: "B2",
+            option1: "avons",
+            option2: "ayons",
+            option3: "avions",
+            option4: "aurons",
+            correctOption: "ayons"
         },
         {
-            "question": "C'est l'entreprise ____ je vous ai parlé hier.",
-            "categ": "Grammaire",
-            "NV": "B2",
-            "option1": "dont",
-            "option2": "où",
-            "option3": "que",
-            "option4": "laquelle",
-            "correctOption": "dont"
+            question: "C'est l'entreprise ____ je vous ai parlé hier.",
+            categ: "Grammaire",
+            NV: "B2",
+            option1: "dont",
+            option2: "où",
+            option3: "que",
+            option4: "laquelle",
+            correctOption: "dont"
         },
         {
-            "question": "Je chercherai quelqu'un qui ____ (pouvoir) m'aider dans ce projet.",
-            "categ": "Grammaire",
-            "NV": "B2",
-            "option1": "pouvait",
-            "option2": "peut",
-            "option3": "puisse",
-            "option4": "pourra",
-            "correctOption": "puisse"
+            question: "Je chercherai quelqu'un qui ____ (pouvoir) m'aider dans ce projet.",
+            categ: "Grammaire",
+            NV: "B2",
+            option1: "pouvait",
+            option2: "peut",
+            option3: "puisse",
+            option4: "pourra",
+            correctOption: "puisse"
         },
         {
-            "question": "Elle agit comme si elle ____ (être) la patronne.",
-            "categ": "Grammaire",
-            "NV": "B2",
-            "option1": "est",
-            "option2": "sera",
-            "option3": "était",
-            "option4": "soit",
-            "correctOption": "était"
+            question: "Elle agit comme si elle ____ (être) la patronne.",
+            categ: "Grammaire",
+            NV: "B2",
+            option1: "est",
+            option2: "sera",
+            option3: "était",
+            option4: "soit",
+            correctOption: "était"
         },
         {
-            "question": "Ils se sont retrouvés dans une situation ____ ils ne pouvaient pas sortir.",
-            "categ": "Grammaire",
-            "NV": "B2",
-            "option1": "que",
-            "option2": "dont",
-            "option3": "où",
-            "option4": "laquelle",
-            "correctOption": "dont"
+            question: "Ils se sont retrouvés dans une situation ____ ils ne pouvaient pas sortir.",
+            categ: "Grammaire",
+            NV: "B2",
+            option1: "que",
+            option2: "dont",
+            option3: "où",
+            option4: "laquelle",
+            correctOption: "dont"
         },
         {
-            "question": "Je suis venu afin que vous ____ (comprendre) mieux la situation.",
-            "categ": "Grammaire",
-            "NV": "B2",
-            "option1": "comprenez",
-            "option2": "comprendrez",
-            "option3": "comprissiez",
-            "option4": "compreniez",
-            "correctOption": "compreniez"
+            question: "Je suis venu afin que vous ____ (comprendre) mieux la situation.",
+            categ: "Grammaire",
+            NV: "B2",
+            option1: "comprenez",
+            option2: "comprendrez",
+            option3: "comprissiez",
+            option4: "compreniez",
+            correctOption: "compreniez"
         },
         {
-            "question": "Il regrette que nous ____ (ne pas venir) plus tôt.",
-            "categ": "Grammaire",
-            "NV": "B2",
-            "option1": "ne sommes pas venus",
-            "option2": "ne soyons pas venus",
-            "option3": "ne venions pas",
-            "option4": "ne serions pas venus",
-            "correctOption": "ne soyons pas venus"
+            question: "Il regrette que nous ____ (ne pas venir) plus tôt.",
+            categ: "Grammaire",
+            NV: "B2",
+            option1: "ne sommes pas venus",
+            option2: "ne soyons pas venus",
+            option3: "ne venions pas",
+            option4: "ne serions pas venus",
+            correctOption: "ne soyons pas venus"
         },
         {
-            "question": "____ qu'il m'a dit est surprenant.",
-            "categ": "Grammaire",
-            "NV": "B2",
-            "option1": "Ce que",
-            "option2": "Ce dont",
-            "option3": "Ce qui",
-            "option4": "Ce dont",
-            "correctOption": "Ce que"
+            question: "____ qu'il m'a dit est surprenant.",
+            categ: "Grammaire",
+            NV: "B2",
+            option1: "Ce que",
+            option2: "Ce dont",
+            option3: "Ce qui",
+            option4: "Ce dont",
+            correctOption: "Ce que"
         },
         {
-            "question": "À moins qu'ils ____ (changer) d'avis, nous n'aurons pas de réponse.",
-            "categ": "Grammaire",
-            "NV": "B2",
-            "option1": "changent",
-            "option2": "changent pas",
-            "option3": "changeons",
-            "option4": "changeront",
-            "correctOption": "changent"
+            question: "À moins qu'ils ____ (changer) d'avis, nous n'aurons pas de réponse.",
+            categ: "Grammaire",
+            NV: "B2",
+            option1: "changent",
+            option2: "changent pas",
+            option3: "changeons",
+            option4: "changeront",
+            correctOption: "changent"
         }
-    ]
     
     
+    //============*****C1*****==========//
+        {
+            question: "Il est dommage que tu ____ (ne pas comprendre) cette explication.",
+            categ: "Grammaire",
+            NV: "C1",
+            option1: "ne comprends",
+            option2: "ne comprennes",
+            option3: "ne comprenne",
+            option4: "ne comprenais",
+            correctOption: "ne comprennes"
+        },
+        {
+            question: "Si j'avais su qu'il ____ (venir), j'aurais préparé quelque chose.",
+            categ: "Grammaire",
+            NV: "C1",
+            option1: "viendra",
+            option2: "soit venu",
+            option3: "vienne",
+            option4: "serait venu",
+            correctOption: "soit venu"
+        },
+        {
+            question: "Je doute qu'il ____ (pouvoir) réussir cet examen sans révision.",
+            categ: "Grammaire",
+            NV: "C1",
+            option1: "puisse",
+            option2: "peut",
+            option3: "pourra",
+            option4: "puise",
+            correctOption: "puisse"
+        },
+        {
+            question: "C'est une personne avec ____ je travaille depuis des années.",
+            categ: "Grammaire",
+            NV: "C1",
+            option1: "laquelle",
+            option2: "qui",
+            option3: "que",
+            option4: "dont",
+            correctOption: "laquelle"
+        },
+        {
+            question: "Je n'ai pas bien compris pourquoi elle ____ (partir) si soudainement.",
+            categ: "Grammaire",
+            NV: "C1",
+            option1: "part",
+            option2: "partait",
+            option3: "est partie",
+            option4: "soit partie",
+            correctOption: "soit partie"
+        },
+        {
+            question: "Il est essentiel que vous ____ (être) présents à la réunion de demain.",
+            categ: "Grammaire",
+            NV: "C1",
+            option1: "soyez",
+            option2: "êtes",
+            option3: "seriez",
+            option4: "seront",
+            correctOption: "soyez"
+        },
+        {
+            question: "Elle ne croit pas qu'ils ____ (avoir) suffisamment d'arguments.",
+            categ: "Grammaire",
+            NV: "C1",
+            option1: "ont",
+            option2: "aient",
+            option3: "aient eu",
+            option4: "avaient",
+            correctOption: "aient"
+        },
+        {
+            question: "Le film était captivant, bien que l'intrigue ____ (être) prévisible.",
+            categ: "Grammaire",
+            NV: "C1",
+            option1: "soit",
+            option2: "était",
+            option3: "soit été",
+            option4: "a été",
+            correctOption: "soit"
+        },
+        {
+            question: "Il ne m'a pas dit s'il ____ (venir) à la fête ce soir.",
+            categ: "Grammaire",
+            NV: "C1",
+            option1: "viendra",
+            option2: "vient",
+            option3: "viendrait",
+            option4: "vienne",
+            correctOption: "viendrait"
+        },
+        {
+            question: "Je n'ai pas de nouvelles de Paul, ____ il m'a promis de m'envoyer.",
+            categ: "Grammaire",
+            NV: "C1",
+            option1: "ce dont",
+            option2: "qui",
+            option3: "ce que",
+            option4: "dont",
+            correctOption: "ce qu'"
+        }
+    //===========*****C2*****===========//
+    {
+        question: "Bien qu'il ____ (avoir) toutes les informations, il n'a rien dit.",
+        categ: "Grammaire",
+        NV: "C2",
+        option1: "a",
+        option2: "ait",
+        option3: "avait",
+        option4: "aura",
+        correctOption: "ait"
+    },
+    {
+        question: "Je n'aurais jamais cru qu'il ____ (réussir) à ce point.",
+        categ: "Grammaire",
+        NV: "C2",
+        option1: "réussira",
+        option2: "réussisse",
+        option3: "réussi",
+        option4: "réussissait",
+        correctOption: "réussisse"
+    },
+    {
+        question: "Il se demande si nous ____ (être) prêts pour le projet.",
+        categ: "Grammaire",
+        NV: "C2",
+        option1: "sommes",
+        option2: "serons",
+        option3: "étions",
+        option4: "serions",
+        correctOption: "sommes"
+    },
+    {
+        question: "Je ne crois pas que cette solution ____ (convaincre) tout le monde.",
+        categ: "Grammaire",
+        NV: "C2",
+        option1: "convainc",
+        option2: "convainque",
+        option3: "convaincait",
+        option4: "convaincra",
+        correctOption: "convainque"
+    },
+    {
+        question: "Il s'est demandé pourquoi elle ____ (partir) sans prévenir.",
+        categ: "Grammaire",
+        NV: "C2",
+        option1: "partait",
+        option2: "soit partie",
+        option3: "partie",
+        option4: "partira",
+        correctOption: "soit partie"
+    },
+    {
+        question: "Quoi qu'il ____ (faire), il n'a jamais abandonné.",
+        categ: "Grammaire",
+        NV: "C2",
+        option1: "fasse",
+        option2: "faisait",
+        option3: "a fait",
+        option4: "fait",
+        correctOption: "fasse"
+    },
+    {
+        question: "Elle lui a demandé s'il ____ (voir) le film hier soir.",
+        categ: "Grammaire",
+        NV: "C2",
+        option1: "a vu",
+        option2: "avait vu",
+        option3: "voit",
+        option4: "verra",
+        correctOption: "avait vu"
+    },
+    {
+        question: "Je serais étonné que vous ____ (être) prêts avant midi.",
+        categ: "Grammaire",
+        NV: "C2",
+        option1: "êtes",
+        option2: "soyez",
+        option3: "étiez",
+        option4: "seriez",
+        correctOption: "soyez"
+    },
+    {
+        question: "Nous aurions préféré que vous ____ (arriver) plus tôt.",
+        categ: "Grammaire",
+        NV: "C2",
+        option1: "arriviez",
+        option2: "arrivez",
+        option3: "arriviez",
+        option4: "arrivâtes",
+        correctOption: "arriviez"
+    },
+    {
+        question: "Je suis convaincu qu'il ____ (accepter) l'offre.",
+        categ: "Grammaire",
+        NV: "C2",
+        option1: "acceptera",
+        option2: "accepte",
+        option3: "a accepté",
+        option4: "accepterait",
+        correctOption: "acceptera"
+    }
+
